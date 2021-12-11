9f4f25e13aa1a70b29dcee25239e1418bc0d21fb
Erick Rosa <erickpmotta@gmail.com>
2021-12-11 17:28:45 -0300

    added some js

--- assets/app.js
+++ assets/app.js
@@ -23,10 +23,23 @@ function getInputValue() {
     let merchName = document.getElementById('name').value;
     let merchValue = document.getElementById('number').value;
     
+
+
+    if (merchName === "") {
+        
+        document.getElementById('merch').innerHTML = "Nenhuma transação cadastrada";
+          document.getElementById('value').innerHTML = "R$ " + merchValue;
+    }
+
+    else if(merchValue === "") {
+        document.getElementById('value').innerHTML = "Nenhum valor cadastrado";
+         document.getElementById('merch').innerHTML = merchName;
+    }
+
+    else {
     document.getElementById('merch').innerHTML = merchName;
     document.getElementById('value').innerHTML = "R$ " + merchValue;
-
- 
+    }
 
 }
 
