cdb00a700551675a7491d830ba0b62289cbd04c9
Erick Rosa <erickpmotta@gmail.com>
2021-12-11 18:24:40 -0300

    js code fix

--- assets/app.js
+++ assets/app.js
@@ -1,47 +1,27 @@
-const hamburguer = document.getElementById('hamburguer');
-const asideBar = document.getElementById('aside');
-const x = document.getElementById('close');
+const hamburguer = document.getElementById("hamburguer");
+const asideBar = document.getElementById("aside");
+const x = document.getElementById("close");
 
+x.addEventListener("click", () => {
+  asideBar.classList.remove("show");
+});
 
-
-x.addEventListener('click', () => {
-    asideBar.classList.remove('show');
-
-})
-
-hamburguer.addEventListener('click', () => {
-    asideBar.classList.toggle('show');
-
-})
-
-
-
-
+hamburguer.addEventListener("click", () => {
+  asideBar.classList.toggle("show");
+});
 
 function getInputValue() {
-    
-    let merchName = document.getElementById('name').value;
-    let merchValue = document.getElementById('number').value;
-    
+  let merchName = document.getElementById("name").value;
+  let merchValue = document.getElementById("number").value;
 
-
-    if (merchName === "") {
-        
-        document.getElementById('merch').innerHTML = "Nenhuma transação cadastrada";
-          document.getElementById('value').innerHTML = "R$ " + merchValue;
-    }
-
-    else if(merchValue === "") {
-        document.getElementById('value').innerHTML = "Nenhum valor cadastrado";
-         document.getElementById('merch').innerHTML = merchName;
-    }
-
-    else {
-    document.getElementById('merch').innerHTML = merchName;
-    document.getElementById('value').innerHTML = "R$ " + merchValue;
-    }
-
+  if (merchName === "") {
+    document.getElementById("merch").innerHTML = "Nenhuma transação cadastrada";
+    document.getElementById("value").innerHTML = "R$ " + merchValue;
+  } else if (merchValue === "") {
+    document.getElementById("value").innerHTML = "Nenhum valor cadastrado";
+    document.getElementById("merch").innerHTML = merchName;
+  } else {
+    document.getElementById("merch").innerHTML = merchName;
+    document.getElementById("value").innerHTML = "R$ " + merchValue;
+  }
 }
-
-
-
