2736563b46a108934567b3a8bbdd83f4cef96cc9
Erick Rosa <erickpmotta@gmail.com>
2022-01-18 16:22:36 -0300

    updating javascript

--- assets/app.js
+++ assets/app.js
@@ -1,3 +1,4 @@
+// Hamburguer Menu
 const hamburguer = document.getElementById("hamburguer");
 const asideBar = document.getElementById("nav");
 const x = document.getElementById("close");
@@ -10,18 +11,99 @@ hamburguer.addEventListener("click", () => {
   asideBar.classList.toggle("show");
 });
 
-function getInputValue() {
-  let merchName = document.getElementById("name").value;
-  let merchValue = document.getElementById("number").value;
+//add extract local storage
+const extractionsRaw = localStorage.getItem("transactions");
 
-  if (merchName === "") {
-    document.getElementById("merch").innerHTML = "Nenhuma transação cadastrada";
-    document.getElementById("value").innerHTML = "R$ " + merchValue;
-  } else if (merchValue === "") {
-    document.getElementById("value").innerHTML = "Nenhum valor cadastrado";
-    document.getElementById("merch").innerHTML = merchName;
-  } else {
-    document.getElementById("merch").innerHTML = merchName;
-    document.getElementById("value").innerHTML = "R$ " + merchValue;
+if (extractionsRaw != null) {
+  var transactions = JSON.parse(extractionsRaw);
+} else {
+  var transactions = [];
+}
+
+//Get Input Value - send to transactions
+
+function getInputValue(e) {
+  e.preventDefault();
+
+  console.log(transactions);
+
+  transactions.push({
+    mercadoria: e.target.elements["name"].value,
+    valor: e.target.elements["number"].value,
+    tipo: e.target.elements['type'].value
+  });
+
+  localStorage.setItem("transactions", JSON.stringify(transactions));
+  drawTransaction();
+}
+
+
+
+//Draw Transactions
+function drawTransaction() {
+  currentLines = [...document.querySelectorAll(".spacebetween")];
+  currentLines.forEach((element) => {
+    element.remove();
+  });
+
+  for (mercadoria in transactions) {
+    if (transactions === []) {
+      document.querySelector(".extract-section").innerHTML += `
+       <div class="spacebetween">
+        <div class="merch-name">
+          <span id="merch">Nenhuma transacao cadastrada</span>
+        </div>
+    `;
+    } else {
+      document.querySelector(".extract-section").innerHTML += `
+  <div>
+   <div class="spacebetween">
+        <div class="merch-name">
+          <span>${ 
+            transactions[mercadoria].tipo == 'buy' ?  '<span>-</span>' : '<span>+</span>'
+    }</span >
+          <span id="merch">${transactions[mercadoria].mercadoria}</span>
+        </div>
+        <span id="value">${transactions[mercadoria].valor}</span>
+      </div>
+        </div>
+  
+  `;
+    }
+
+
+  }
+}
+
+function deleteValue(p) {
+  transactions.splice(p);
+  drawTransaction();
+  document.querySelector(".extract-section").innerHTML += `
+       <div class="spacebetween">
+        <div class="merch-name">
+         
+          <span id="merch">Nenhuma transacao cadastrada</span>
+        </div>
+    `;
+  localStorage.setItem("transactions", JSON.stringify(transactions));
+}
+
+drawTransaction();
+
+//form test validation
+
+function formValidation(e) {
+  e.preventDefault();
+
+  if (e.target.value.length == 0) {
+    e.target.value += "R$ ";
+  }
+
+  if (e.target.value.length == 6) {
+    e.target.value += ",";
+  }
+
+  if (/[0-9 ,.]/g.test(e.key)) {
+    e.target.value += e.key;
   }
 }
